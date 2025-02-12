16c099c854b63c7fd4ffa5de772dc2de427d7e8f
Idowu 'hydeenoble' Emehinola <hydeenoble39@gmail.com>
2025-02-12 19:59:52 +0100

    Configured Sentry for frontend app
    Signed-off-by: Idowu 'hydeenoble' Emehinola <hydeenoble39@gmail.com>

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,5 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://b3fbb75388ee23780a9711d171bdc337@o4508807719092224.ingest.us.sentry.io/4508807726694400",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", import.meta.env.VITE_API_URL],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
