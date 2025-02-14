516196bb94cb6cebf9785c5e47f0161f081ab233
Idowu 'hydeenoble' Emehinola <hydeenoble39@gmail.com>
2025-02-14 16:34:19 +0100

    Configured Sentry for Lambda functions
    Signed-off-by: Idowu 'hydeenoble' Emehinola <hydeenoble39@gmail.com>

--- packages/functions/src/handlers/getTasks.ts
+++ packages/functions/src/handlers/getTasks.ts
@@ -1,11 +1,33 @@
 import * as Sentry from "@sentry/aws-serverless";
 
-// TODO: Add more configurationg for Sentry to get more detailed error
-
 import { query } from '../db/dbClient';
 import { Handler } from "aws-lambda";
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+});
+// Manually call startProfiler and stopProfiler
+// to profile the code in between
+Sentry.profiler.startProfiler();
+
+// Starts a transaction that will also be profiled
+Sentry.startSpan({
+  name: "My First Transaction",
+}, () => {
+  // the code executing inside the transaction will be wrapped in a span and profiled
+});
+
+// Calls to stopProfiling are optional - if you don't stop the profiler, it will keep profiling
+// your application until the process exits or stopProfiling is called.
+Sentry.profiler.stopProfiler();
+// Place any other require/import statements here
+
 export const handler: Handler = Sentry.wrapHandler(async (_event) => {
+
+  // throw new Error("This should show up in Sentry!")
 
     try {
     
